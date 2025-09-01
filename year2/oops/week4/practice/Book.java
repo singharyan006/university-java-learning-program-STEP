aecbfe9dff46c530e4689d4a62c03d63f337afa6
singharyan006 <aryanraisingh2006@gmail.com>
2025-09-01 18:11:58 +0530

    feat: add Book class with default and parameterized constructors

---
+++ year2/oops/week4/practice/Book.java
@@ -0,0 +1,39 @@
+public class Book {
+    String title;
+    String author;
+    double price;
+
+    // Default constructor
+    public Book() {
+        this.title = "Unknown Title";
+        this.author = "Unknown Author";
+        this.price = 0.0;
+    }
+
+    // Parameterized constructor
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // Display method
+    public void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: ₹" + price);
+        System.out.println("------------------------");
+    }
+
+    public static void main(String[] args) {
+        // Create book1 using default constructor
+        Book book1 = new Book();
+
+        // Create book2 using parameterized constructor
+        Book book2 = new Book("Clean Code", "Robert C. Martin", 599.99);
+
+        // Display both books
+        book1.display();
+        book2.display();
+    }
+}
