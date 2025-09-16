5696bde0cde1d455faed694afb661903a64db81d
singharyan006 <aryanraisingh2006@gmail.com>
2025-09-16 15:45:14 +0530

    feat: add simple multilevel inheritance example with Animal, Mammal, and Dog classes

--- year2/oops/week6/practice/Q2/MultilevelInheritanceDemo.java
+++ year2/oops/week6/practice/Q2/MultilevelInheritanceDemo.java
@@ -1,26 +1,26 @@
-class Animal {
-    void eat() {
-        System.out.println("Animal eats.");
+    class Animal {
+        void eat() {
+            System.out.println("Animal eats.");
+        }
     }
-}
 
-class Mammal extends Animal {
-    void walk() {
-        System.out.println("Mammal walks.");
+    class Mammal extends Animal {
+        void walk() {
+            System.out.println("Mammal walks.");
+        }
     }
-}
 
-class Dog extends Mammal {
-    void bark() {
-        System.out.println("Dog barks.");
+    class Dog extends Mammal {
+        void bark() {
+            System.out.println("Dog barks.");
+        }
     }
-}
 
-public class MultilevelInheritanceDemo {
-    public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.eat();   // from Animal
-        dog.walk();  // from Mammal
-        dog.bark();  // from Dog
+    public class MultilevelInheritanceDemo {
+        public static void main(String[] args) {
+            Dog dog = new Dog();
+            dog.eat();   // from Animal
+            dog.walk();  // from Mammal
+            dog.bark();  // from Dog
+        }
     }
-}
