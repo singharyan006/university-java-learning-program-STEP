3368bf71698243a838c361322c7dc60e387e7696
Aryan Singh <aryanraisingh2006@gmail.com>
2026-02-21 09:00:00 +0530

    UC4 - Character Array Based Palindrome Check

---
+++ year-II/sem-IV/palindrome/uc4/PalindromeCheckerApp.java
@@ -0,0 +1,30 @@
+public class PallindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "madam";
+
+        char[] arr = input.toCharArray();   // convert String → char[]
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (left < right) {
+
+            if (arr[left] != arr[right]) {
+                isPalindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+
+}
