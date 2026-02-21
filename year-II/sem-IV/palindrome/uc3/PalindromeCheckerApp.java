f5989b30d9ac218e1a7f0128f0472c193ce56149
Aryan Singh <aryanraisingh2006@gmail.com>
2026-02-21 09:00:00 +0530

    UC3 - Palindrome Check Using String Reverse

---
+++ year-II/sem-IV/palindrome/uc3/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+public class PallindromeCheckerApp {
+    public static void main(String[] args) {
+        String ip = "madam";
+        String r_ip = "";
+        for(int i=ip.length()-1; i>=0; i--) {
+            r_ip += String.valueOf(ip.charAt(i));
+        }
+
+        if(ip.equals(r_ip)) {
+            System.out.println("ip string is pallindrome");
+        } else {
+            System.out.println("ip string is not a pallindrome");
+        }
+    }
+}
