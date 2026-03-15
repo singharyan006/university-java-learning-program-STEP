1e8e706c69c84b15200fc8d3728e60bc5a1fde69
Aryan Singh <aryanraisingh2006@gmail.com>
2026-03-15 12:00:00 +0530

    Add RoomInventory.java for Use Case 5

---
+++ year-II/sem-IV/bookmystay/uc5/RoomInventory.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RoomInventory {
+    private Map<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+    }
+
+    public void addRoomType(String roomType, int count) {
+        inventory.put(roomType, count);
+    }
+
+    public int getAvailableRooms(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
+
+    public void updateRoomAvailability(String roomType, int count) {
+        if (inventory.containsKey(roomType)) {
+            inventory.put(roomType, count);
+        } else {
+            System.out.println("Room type '" + roomType + "' not found in inventory.");
+        }
+    }
+
+    public void displayInventory() {
+        System.out.println("\n--- Current Room Inventory ---");
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue() + " available");
+        }
+        System.out.println("------------------------------");
+    }
+}
